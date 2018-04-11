9e80bcc1af798acb27a282a1035d723d05aa25fe
admin-PC\admin <le.van.hai@miyatsu.vn>
2018-04-11 22:08:35 +0700

    update

--- server/laravel/weechat/routes/web.php
+++ server/laravel/weechat/routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+Route::group(['middleware' => ['auth', 'setLocale']], function () {
+
+    Route::get('/account/account-information', 'UserController@accountInformation');
+    Route::post('/account/account-information', 'UserController@updateAccountInformation');
+    Route::get('/account/edit', 'UserController@accountEdit');
+    Route::post('/account/update', 'UserController@accountUpdate');
+    Route::post('/user/getListUser', ['uses' => 'UserController@getListUser', 'as' => 'user.list']);
 });
+Route::get('/', 'Auth\LoginController@showLoginForm');
+Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('/auth/register', 'Auth\RegisterController@showMailRegisterForm');
+Route::post('/auth/register', 'Auth\RegisterController@sendRegisterLink');
+Route::get('/auth/confirm/{token}', 'Auth\RegisterController@showRegisterForm');
+Route::post('/auth/register/create', 'Auth\RegisterController@register');
